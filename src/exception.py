ab32c1dc776d066337413412c0d47d696c8dce49
Utkarsh-Singh-u <utkarshsinghgkp2004@gmail.com>
2025-06-30 14:57:29 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
   _, _, exc_tb = error_detail.exc_info()
@@ -9,7 +9,7 @@ def error_message_detail(error, error_detail:sys):
   )
   return error_message
 
-class CustonException(Exception):
+class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail=error_detail)
@@ -22,4 +22,4 @@ if __name__ == "__main__":
      a = 10 / 0
   except Exception as e:
      logging.info("Divide by Zero Error") 
-     raise CustonException(e,sys)
+     raise CustomException(e,sys)
--- src/logger.py
+++ src/logger.py
@@ -2,14 +2,18 @@ import logging
 import os
 from datetime import datetime
 
+
+# Create logs directory path
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# Define full log file path
 LOG_FILE = f"{datetime.now().strftime('%m-%d-%Y_%H-%M-%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
 
-LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
-
+# Configure logging
 logging.basicConfig(
-  filename=LOG_FILE_PATH,
-  format="[ %(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s ",
-  level=logging.INFO,
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
